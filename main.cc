555eb84416b06d48899ff791cd9f024fed1ba0a8
Rizler2098 <116823588+Rizler2098@users.noreply.github.com>
2022-10-28 17:23:21 +0100

    update 28.10.22
    setting up code from basics

--- main.cc
+++ main.cc
@@ -1,1 +1,34 @@
+#include <iostream>
+#include ≤string≥
+#include <fstream>
 
+using namespace std;
+
+class file_handle{
+  string _extension = ".txt";
+  string filename = "default;
+  string directory = "./noname";
+  
+  void append_extension();
+  int read();
+  int write();
+ public:
+  string buffer = nullptr;
+  int get(&content);
+  int overwrite(&content);
+  int append(&content)
+  
+}
+
+//FUNCTIONS DEFINTIONS
+using username file_handle;
+
+//basic functionality
+int read(){
+  
+  
+}
+int write(){
+  
+}
+//END OF DEFINITIONS
